6c47598e03605c5924fb1ebc92baa66064004696
Michał Ossowski <michalo2882@gmail.com>
2020-02-22 21:15:22 +0100

    add some serializers and dashboard view

--- app/rest/serializers.py
+++ app/rest/serializers.py
@@ -3,7 +3,31 @@ from rest_framework import serializers
 from app.models import *
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ['user', 'coins']
+
+
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['name']
+
+
+class InventoryItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItem
+        fields = ['user', 'item', 'count', 'description']
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ['item', 'count', 'price', 'direction', 'submitter', 'description']
--- app/rest/views.py
+++ app/rest/views.py
@@ -10,7 +10,16 @@ from app.rest.serializers import *
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def dashboard(request):
-    return Response({'user': request.user.username})
+    sell_listings = Listing.objects.filter(submitter=request.user, direction=Listing.Direction.SELL).order_by('-pk')
+    buy_listings = Listing.objects.filter(submitter=request.user, direction=Listing.Direction.BUY).order_by('-pk')
+    inventory_items = InventoryItem.objects.filter(user=request.user)
+    return Response({
+        'user': request.user.username,
+        'wallet': WalletSerializer(Wallet.get_users_wallet(request.user)).data,
+        'sellListings': ListingSerializer(sell_listings, many=True).data,
+        'buyListings': ListingSerializer(buy_listings, many=True).data,
+        'inventory': InventoryItemSerializer(inventory_items, many=True).data,
+    })
 
 
 class ItemViewSet(mixins.RetrieveModelMixin,
