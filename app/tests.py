8a13a77c9888ea7858f7b63657a303fcd40f3476
Michał Ossowski <michalo2882@gmail.com>
2020-02-22 13:31:28 +0100

    add user listings and items to dashboard

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-from app.models import Wallet, Item, InventoryItem
+from app.models import Wallet, Item, InventoryItem, Listing
 
 
 class WalletTests(TestCase):
@@ -20,3 +21,31 @@ class ItemTests(TestCase):
         user = User.objects.create_user(username='ben', password='abc')
         item.add_to_user_inventory(user, count=20)
         self.assertEquals(20, InventoryItem.objects.get(user=user, item=item).count)
+
+
+class DashboardViewTests(TestCase):
+    def setUp(self):
+        self.item = Item.objects.create(name='sword')
+        self.user = User.objects.create_user(username='ben', password='abc')
+
+    def test_unauthorized(self):
+        response = self.client.get(reverse('app:dashboard'))
+        self.assertRedirects(response, '/accounts/login/?next=/')
+
+    def test_no_listings(self):
+        self.client.login(username='ben', password='abc')
+        response = self.client.get(reverse('app:dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['sell_listings'], [])
+        self.assertQuerysetEqual(response.context['buy_listings'], [])
+
+    def test_listings(self):
+        self.client.login(username='ben', password='abc')
+        sell = Listing.objects.create(item=self.item, count=10, price=20, direction=Listing.Direction.SELL,
+                                      submitter=self.user)
+        buy = Listing.objects.create(item=self.item, count=30, price=50, direction=Listing.Direction.BUY,
+                                     submitter=self.user)
+        response = self.client.get(reverse('app:dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['sell_listings'], [repr(sell)])
+        self.assertQuerysetEqual(response.context['buy_listings'], [repr(buy)])
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 
-from app.models import Wallet, Item
+from app.models import Wallet, Item, Listing, InventoryItem
 
 
 class ItemsView(generic.ListView):
@@ -12,6 +12,12 @@ class ItemsView(generic.ListView):
 
 @login_required
 def dashboard(request):
+    sell_listings = Listing.objects.filter(submitter=request.user, direction=Listing.Direction.SELL).order_by('-pk')
+    buy_listings = Listing.objects.filter(submitter=request.user, direction=Listing.Direction.BUY).order_by('-pk')
+    inventory_items = InventoryItem.objects.filter(user=request.user)
     return render(request, 'app/dashboard.html', {
-        'wallet': Wallet.get_users_wallet(request.user)
+        'wallet': Wallet.get_users_wallet(request.user),
+        'sell_listings': sell_listings,
+        'buy_listings': buy_listings,
+        'inventory_items': inventory_items,
     })
